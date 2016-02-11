75cda2a7ee572ffb41fe55846e26bd46d5764022
Jerry.Gomez <Jerry.Gomez@caribmedia.com>
2016-02-11 13:35:44 -0400

    Implementing the options page

--- options/options.js
+++ options/options.js
@@ -1,37 +1,26 @@
 
 function save(){
 
-  var sunday_service = document.getElementById('sunday_service').checked;
-  var roast_n_toast = document.getElementById('roast_n_toast').checked;
-  var movie_review_extravaganza = document.getElementById('movie_review_extravaganza').checked;
-  var movie_review = document.getElementById('movie_review').checked;
-
+  var title_selector = document.getElementById('title_selector').value;
   chrome.storage.sync.set({
-    sunday_service: sunday_service,
-    roast_n_toast:roast_n_toast,
-    movie_review_extravaganza:movie_review_extravaganza,
-    movie_review:movie_review
+    title_selector: title_selector,
   }, function() {
-  
+
     });
 }
 
 function restore_options() {
   chrome.storage.sync.get({
-    sunday_service: true,
-    roast_n_toast:true,
-    movie_review_extravaganza:true,
-    movie_review:true
+    title_selector: ".mainContent .viewListingPage h1",
   }, function(items) {
-    document.getElementById('sunday_service').checked = items.sunday_service ;
-    document.getElementById('roast_n_toast').checked = items.roast_n_toast;
-    document.getElementById('movie_review_extravaganza').checked = items.movie_review_extravaganza;
-    document.getElementById('movie_review').checked = items.movie_review;
+      document.getElementById('title_selector').value =items.title_selector;
+
   });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-var el = document.getElementsByClassName('cmn-toggle');
+
+var el = document.getElementsByClassName('cmn-button');
 for (var i = 0; i < el.length; i++) {
      var obj = el[i];
      obj.addEventListener('click',save);
